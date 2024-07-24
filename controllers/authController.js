b85c13e3cb4528a389e2c1cb088505103436eb66
lakshyasuthar <lakshyasuthar.freelance@gmail.com>
2024-07-24 16:21:30 +0530

    bug fix

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,8 +3,6 @@ import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 process.on("unhandledRejection", (reason, promise) => {
   console.log("Unhandled Rejection at:", reason.stack || reason);
-  // Recommended: send the information to sentry.io
-  // or whatever crash reporting service you use
 });
 export async function register(req, res) {
   try {
@@ -20,70 +18,64 @@ export async function register(req, res) {
       linkedin: "",
       github: "",
     };
+
     if (await userModel.findOne({ email: email })) {
       return res.json({
         message: "email already present",
         status: "error",
       });
-    } else {
+    }
+    if (await userModel.findOne({ handle: handle })) {
+      return res.json({
+        message: "handle already present",
+        status: "error",
+      });
+    }
+
+    if (email && password && handle) {
       try {
-        if (await userModel.findOne({ handle: handle })) {
-          return res.json({
-            message: "handle already present",
-            status: "error",
-          });
-        }
-      } catch (error) {
-        console.log(error);
-      }
-      if (email && password && handle) {
-        try {
-          bcrypt.hash(password, 8, async function (err, hashedPassword) {
-            const user = await userModel.create({
-              email: email,
-              password: hashedPassword,
-              handle: handle,
-              link: [defaultLink],
-              social: [defaultSocialLinks],
-              themes: [
-                {
-                  id: 1,
-                  name: "default",
-                  colors: ["#1d1d1d", "#1d1d1d", "#cccccc"],
-                },
-                {
-                  id: 2,
-                  name: "theme1",
-                  colors: ["#2c3e50", "#f0f0f0", "#1abc9c"],
-                },
-              ],
-              currentTheme: {
+        bcrypt.hash(password, 8, async function (err, hashedPassword) {
+          const user = await userModel.create({
+            email: email,
+            password: hashedPassword,
+            handle: handle,
+            link: [defaultLink],
+            social: [defaultSocialLinks],
+            themes: [
+              {
                 id: 1,
                 name: "default",
                 colors: ["#1d1d1d", "#1d1d1d", "#cccccc"],
               },
-            });
-            const token = jwt.sign({ email: email }, "SECRET_KEY");
-            return res.json({
-              message: "user created",
-              status: "success",
-              token: token,
-              id: user._id,
-            });
+              {
+                id: 2,
+                name: "theme1",
+                colors: ["#2c3e50", "#f0f0f0", "#1abc9c"],
+              },
+            ],
+            currentTheme: {
+              id: 1,
+              name: "default",
+              colors: ["#1d1d1d", "#1d1d1d", "#cccccc"],
+            },
           });
-        } catch (error) {
+          const token = jwt.sign({ email: email }, "SECRET_KEY");
           return res.json({
-            message: error.message,
-            status: "error",
+            message: "user created",
+            status: "success",
+            token: token,
+            id: user._id,
           });
-        }
+        });
+      } catch (error) {
+        return res.json({
+          message: error.message,
+          status: "error",
+        });
       }
     }
   } catch (error) {
-    return res.json({
-      message: error.message,
-      status: "error",
-    });
+    return res.json({ message: error.message, status: "error" });
   }
 }
 export async function login(req, res) {
